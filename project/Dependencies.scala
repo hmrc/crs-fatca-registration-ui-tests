aeae1fb70ebddfb3a6d62489f41dd8e3bb95065d
MikePratt <29142504+Bongowhack@users.noreply.github.com>
2024-03-07 17:35:10 +0000

    "Bumped dependences, updated README for sm2 arguments and included file for excluding URLs from A11y tests."

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,12 +2,12 @@ import sbt._
 
 object Dependencies {
 
-  val test = Seq(
-    "com.vladsch.flexmark" % "flexmark-all"   % "0.64.8"        % Test,
-    "org.scalatest"       %% "scalatest"      % "3.2.18"        % Test,
-    "org.slf4j"            % "slf4j-simple"   % "2.0.9"         % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner" % "0.19.0"        % Test,
-    "uk.gov.hmrc"         %% "domain"         % "8.3.0-play-28" % Test
+  val test: Seq[ModuleID] = Seq(
+    "ch.qos.logback"       % "logback-classic" % "1.5.1"         % Test,
+    "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8"        % Test,
+    "org.scalatest"       %% "scalatest"       % "3.2.18"        % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.21.0"        % Test,
+    "uk.gov.hmrc"         %% "domain"          % "8.3.0-play-28" % Test
   )
 
 }
