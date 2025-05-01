49fb5df32703a1b015beb69b8242e68aac10ea9a
Carl Thompson <carl.thompson@opencastsoftware.com>
2025-05-01 09:08:28 +0100

    update versions/deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,10 +3,10 @@ import sbt._
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "ch.qos.logback"       % "logback-classic" % "1.5.1"  % Test,
+    "ch.qos.logback"       % "logback-classic" % "1.5.18"  % Test,
     "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8" % Test,
-    "org.scalatest"       %% "scalatest"       % "3.2.18" % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.43.0" % Test,
+    "org.scalatest"       %% "scalatest"       % "3.2.19" % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.45.0" % Test,
     "uk.gov.hmrc"         %% "domain-play-30"  % "10.0.0" % Test
   )
 
